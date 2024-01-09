035e2a16f62e00f356115fb08f7a752461f8eebc
lintang81 <lintangkeren08@gmail.com>
2024-01-09 18:59:58 +0800

    Menampilkan halaman read lokasi

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -32,7 +32,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="?page=lokasiread" class="nav-link active">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Lokasi</p>
                             </a>
